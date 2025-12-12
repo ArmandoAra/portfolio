6acc9d266915db59c085c093e2ac66fef9a2e05b
ArmandoAra <armand2arano@gmail.com>
2025-12-12 15:50:10 -0300

    Actualizado react a la version 19.2.3

--- src/pages/projects/[name]/page.tsx
+++ src/pages/projects/[name]/page.tsx
@@ -1,9 +1,7 @@
-import { useParams } from "react-router-dom";
 import MyTreeProject from "./myTree";
 
 
 export default function ProjectDescription() {
-    const { name } = useParams();
 
     return (
         <div className="flex flex-col w-[84vw] md:w-full">
